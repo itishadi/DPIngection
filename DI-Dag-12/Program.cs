587b249af2295eb41e9510c513d4b7a4c9f9e95b
Hadi Rasouli <hadirasouli786@hotmail.com>
2022-12-22 10:46:39 +0100

    #1-DEPENDENCY-INJECTION

--- DI-Dag-12/Program.cs
+++ DI-Dag-12/Program.cs
@@ -2,6 +2,6 @@
 
 var user1 = new User("Richard");
 
-user1.ChangeUserName("Richard the Lionheart (With out DEPENDENCY INJECTION)");
+user1.ChangeUserName("Richard the Lionheart (With out DEPENDENCY INJECTION!)");
 
 Console.ReadLine();
